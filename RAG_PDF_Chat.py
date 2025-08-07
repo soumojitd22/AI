9e3fcb80f2607940ae59cad36d3c5d4a40184da4
soumo <40084876+soumojitd22@users.noreply.github.com>
2025-08-07 19:16:32 +0530

    Enhance PDF text extraction and chat functionality with new document processing methods

--- RAG_PDF_Chat.py
+++ RAG_PDF_Chat.py
@@ -2,15 +2,18 @@ import base64
 import os
 from io import BytesIO
 
-from langchain.chains.retrieval_qa.base import RetrievalQA
-from langchain.schema import HumanMessage
-from langchain.text_splitter import CharacterTextSplitter
 from langchain_chroma import Chroma
+from langchain_community.document_loaders.parsers import TesseractBlobParser
+from langchain_core.documents import Document
+from langchain_core.messages import HumanMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 from langchain_ollama import ChatOllama
 from langchain_ollama import OllamaEmbeddings
+from langchain_pymupdf4llm import PyMuPDF4LLMLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from pdf2image import convert_from_path
-
-from ScanPDFUsingPDFLoader import scan_pdf
 
 
 def pdf_pages_to_b64(pdf_path: str, dpi=300):
@@ -69,7 +72,7 @@ def build_ocr_prompt(b64_png: str) -> list:
 
 
 def transcribe_page(b64_png):
-    response = llm.invoke(build_ocr_prompt(b64_png))
+    response = vision_llm.invoke(build_ocr_prompt(b64_png))
     return response.content
 
 
@@ -79,30 +82,82 @@ def extract_text_from_pdf(pdf_path: str) -> str:
     return "\n\n".join(texts)
 
 
+def extract_text_from_docs(docs: list[Document]) -> str:
+    """
+    Extracts text from a list of Document objects.
+
+    Args:
+        docs (list[Document]): List of Document objects.
+
+    Returns:
+        str: Concatenated text from all documents.
+    """
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+def scan_pdf_locally(pdf_path: str) -> list[Document]:
+    """
+    Scans a PDF file and returns a list of Document objects.
+
+    Args:
+        pdf_path (str): Path to the PDF file.
+
+    Returns:
+        list[Document]: List of Document objects containing the scanned text.
+    """
+    loader = PyMuPDF4LLMLoader(pdf_path,
+                               mode="page",
+                               table_strategy="lines",
+                               extract_images=True,
+                               images_parser=TesseractBlobParser())
+    documents = loader.load()
+    return documents
+
+
 if __name__ == "__main__":
     chroma_db_loc = "./chroma_db"
     embeddings = OllamaEmbeddings(model="nomic-embed-text:latest")
     if not os.path.exists(chroma_db_loc):
-        llm = ChatOllama(model="granite3.2-vision:latest", keep_alive="0")
+        vision_llm = ChatOllama(model="granite3.2-vision:latest", keep_alive="0")
         pdf_file = "RRB NTPC.pdf"
         print(f"Extracting text from {pdf_file}...")
         extracted_text = extract_text_from_pdf(pdf_file)
-        splitter = CharacterTextSplitter(chunk_size=5000, chunk_overlap=1000)
-        chunks = splitter.split_text(extracted_text)
+        extracted_text_from_docs = extract_text_from_docs(scan_pdf_locally(pdf_file))
+        text = extracted_text + "\n\n" + extracted_text_from_docs
+        splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50, length_function=len)
+        chunks = splitter.split_text(text)
         vectorstore = Chroma.from_texts(chunks, embeddings, persist_directory=chroma_db_loc)
-        vectorstore.add_documents(scan_pdf(pdf_file))
     else:
         vectorstore = Chroma(persist_directory=chroma_db_loc, embedding_function=embeddings)
 
-    retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
     search_llm = ChatOllama(model="llama3.1:8b", keep_alive="0")
-    rag_chain = RetrievalQA.from_chain_type(llm=search_llm, retriever=retriever)
+    system_prompt = (
+        """
+        You are a helpful AI assistant.
+        You can answer questions based on the provided context.
+        Also you can answer general questions that does not require context.
+        Use three sentence maximum and keep the answer concise.
+        Context: {context}
+        """
+    )
+    prompt_template = ChatPromptTemplate.from_messages([
+        ("system", system_prompt),
+        ("human", "Question: {input}")
+    ])
+
+    chain = (
+            {"context": retriever, "input": RunnablePassthrough()}
+            | prompt_template
+            | search_llm
+            | StrOutputParser()
+    )
+
     while True:
         user_input = input("\n Enter your question (or type 'exit' to quit): \n")
         if user_input.lower() == 'exit':
             print("Exiting the chat. Goodbye!")
             break
 
-        result = rag_chain.invoke(user_input)
-        print("\n** Question **: ", result['query'])
-        print("\n** Answer **: ", result['result'])
+        llm_response = chain.invoke(user_input)
+        print("\n** Answer **: ", llm_response)
